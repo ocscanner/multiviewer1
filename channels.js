a9dd976b6e28affbb48481a3b19f83d1b644322a
ocscanner <ocscannernews@gmail.com>
2026-01-31 20:45:18 -0800

    Restore channels.js global format and remove CBS Los Angeles temporarily

--- channels.js
+++ channels.js
@@ -1,8 +1,63 @@
-const channels = [
-    { name: 'NBC Los Angeles', url: 'https://www.nbc.com/' },
-    { name: 'ABC Los Angeles', url: 'https://abc.com/' },
-    { name: 'Fox Los Angeles', url: 'https://www.fox.com/' },
-    // CBS Los Angeles removed temporarily
-];
+window.APP_CONFIG = {
+  title: "OC Scanner • Multiviewer"
+};
 
-export default channels;
+window.CHANNEL_GROUPS = {
+  local: {
+    label: "Local News",
+    channels: [
+      {
+        name: "FOX (Local)",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/8u8pQ_uLGjo?autoplay=1&mute=1&playsinline=1"
+      },
+      {
+        name: "ABC7 Los Angeles (Official)",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/s3iVFJoxrYc?si=Ci8jUjHc6TR0xCTl&autoplay=1&mute=1&playsinline=1"
+      },
+      {
+        name: "NBC Los Angeles (Official)",
+        type: "iframe",
+        url: "https://www.nbclosangeles.com/watch/"
+      }
+    ]
+  },
+
+  national: {
+    label: "National News",
+    channels: [
+      {
+        name: "NBC News (Live)",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/MINldxbTOSI?si=XOdOYXSKJrmdpNG8&autoplay=1&mute=1&playsinline=1"
+      },
+      {
+        name: "ABC News (National)",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/1qSichtfkjk?autoplay=1&mute=1&playsinline=1"
+      },
+      {
+        name: "FOX Weather",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/wt6SIE7BXS8?autoplay=1&mute=1&playsinline=1"
+      }
+    ]
+  },
+
+  other: {
+    label: "Other (Later)",
+    channels: [
+      {
+        name: "LAX Runways Cam 1",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/VjqKog3XwZ8?autoplay=1&mute=1&playsinline=1"
+      },
+      {
+        name: "LAX Runways Cam 2",
+        type: "youtube",
+        url: "https://www.youtube.com/embed/UQaSS4_VAV4?autoplay=1&mute=1&playsinline=1"
+      }
+    ]
+  }
+};
